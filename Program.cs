e3720d802abd2758130864c51a2752c9f5d61585
Krzysztof <kwaszkiewicz94@gmail.com>
2024-10-03 01:24:29 +0200

    Created initial structure of the application with Clean Architecture principles

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OMDbAPICaller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
